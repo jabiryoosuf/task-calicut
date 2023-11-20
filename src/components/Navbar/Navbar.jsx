b3d34cd3978478bf4252a93d123c12d6f137ffe1
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2023-11-20 14:21:32 +0530

    compleated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import classes from "./navbar.module.css"
+import React from "react";
+import classes from "./navbar.module.css";
+import logo from "../../assets/logo.png";
+import roundLogo from "../../assets/round-logo.png";
+import { FaChevronDown } from "react-icons/fa";
+
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
-  )
-}
+    <nav className={classes.navbar}>
+      <div className={classes.logo_container}>
+        <img className={classes.logo} src={logo} alt="Logo" />
+        <div className={classes.triangle}></div>
+      </div>
+      <div className={classes.company_container}>
+      <div className={classes.company}>
+        <img className={classes.roundLogo} src={roundLogo} alt="logo" />
+        XYZ Enterprizes Pvt.Ltd
+      </div>
+      <div className={classes.showmore}><FaChevronDown /></div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      sodebar
+    </div>
+  )
+}
+
+export default Sidebar
